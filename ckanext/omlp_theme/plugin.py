115e2c2b7cbc8988b21acee0607c025c2f01ee14
FacundoAdorno <adorno.facundo@gmail.com>
2019-10-24 13:44:14 -0300

    -#6170 Se agrego snippet para cargar GoogleAnalytics
    Se tiene que agregar la propiedad "omlp_theme.googleanalytics.id" en el
    archivo de configuracion pertinente (p.e. production.ini).
    
    P.e.:
    -### Google Analytics ID for OMLP Theme
    omlp_theme.googleanalytics.id = UA-1010101-1

--- ckanext/omlp_theme/plugin.py
+++ ckanext/omlp_theme/plugin.py
@@ -1,13 +1,26 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from pylons import config
 
+
+def getGoogleAnalyticsID():
+    ##Load GoogleAnalytics ID into config
+    return config.get('omlp_theme.googleanalytics.id', '')
 
 class Omlp_ThemePlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
-
     # IConfigurer
+    plugins.implements(plugins.IConfigurer)
+    # Declare that this plugin will implement ITemplateHelpers.
+    plugins.implements(plugins.ITemplateHelpers)
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'omlp_theme')
+
+    def get_helpers(self):
+        # Template helper function names should begin with the name of the
+        # extension they belong to, to avoid clashing with functions from
+        # other extensions.
+        return {'omlp_theme_googleanalytics_id': getGoogleAnalyticsID}
+
